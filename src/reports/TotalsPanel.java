bc736a35af5067db43808d5c904b518229fab5a5
mikeburkat <mike.burkat@gmail.com>
2014-07-09 18:36:55 -0400

    Total table choices of products and cities implemented

--- src/reports/TotalsPanel.java
+++ src/reports/TotalsPanel.java
@@ -1,8 +1,11 @@
 package reports;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -11,7 +14,7 @@ import javax.swing.table.DefaultTableModel;
 import net.miginfocom.swing.MigLayout;
 import database.DataBase;
 
-public class TotalsPanel extends JPanel {
+public class TotalsPanel extends JPanel implements ActionListener {
 
 	String date;
 	String[] columns = {"quantity", "Product"};
@@ -31,20 +34,47 @@ public class TotalsPanel extends JPanel {
 	JButton montreal;
 	JButton ottawa;
 	
+	JLabel selection;
+	
 
 	public TotalsPanel() {
 		db = DataBase.getInstance();
 		
 		MigLayout mig = new MigLayout();
-		mig.setColumnConstraints("[grow][grow][grow][grow]");
-		mig.setRowConstraints("20[grow][grow][grow][grow][grow][grow]");
+		mig.setColumnConstraints("[grow][grow][grow]");
+		mig.setRowConstraints("[grow][grow][grow][grow][grow][grow]");
 		
 		this.setLayout(mig);
+		
 		tableModel = new DefaultTableModel(columns, 0);
 		sp = new JScrollPane();
 		table = new JTable(tableModel);
 		sp = new JScrollPane(table);
-		this.add(sp, "cell 0 0 4 6, center");
+		this.add(sp, "cell 0 1 3 5, center");
+		
+		allProducts = new JButton("Piek + Cuk");
+		bakery = new JButton("Piekarnia");
+		pastry = new JButton("Cukiernia");
+		allCities = new JButton("MTL + OTT");
+		montreal = new JButton("Montreal");
+		ottawa = new JButton("Ottawa");
+		
+		allProducts.addActionListener(this);
+		bakery.addActionListener(this);
+		pastry.addActionListener(this);
+		allCities.addActionListener(this);
+		montreal.addActionListener(this);
+		ottawa.addActionListener(this);
+		
+		this.add(allProducts, "cell 0 5, center");
+		this.add(bakery, "cell 1 5, center");
+		this.add(pastry, "cell 2 5, center");
+		this.add(allCities, "cell 0 6, center");
+		this.add(montreal, "cell 1 6, center");
+		this.add(ottawa, "cell 2 6, center");
+		
+		selection = new JLabel(productChoice + " | " + cityChoice);
+		this.add(selection, "cell 1 0, center");
 		
 	}
 
@@ -63,6 +93,48 @@ public class TotalsPanel extends JPanel {
 		table.setModel(tableModel);
 		
 		this.repaint();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+		case "Piek + Cuk":
+			productChoice = bread + ", " + cake;
+			selection.setText(productChoice + " | " + cityChoice);
+			update();
+			System.out.println(e.getActionCommand());
+			break;
+		case "Piekarnia":
+			productChoice = bread;
+			selection.setText(productChoice + " | " + cityChoice);
+			update();
+			System.out.println(e.getActionCommand());
+			break;
+		case "Cukiernia":
+			productChoice = cake;
+			selection.setText(productChoice + " | " + cityChoice);
+			update();
+			System.out.println(e.getActionCommand());
+			break;
+		case "MTL + OTT":
+			cityChoice = "'Montreal', 'Ottawa'";
+			selection.setText(productChoice + " | " + cityChoice);
+			update();
+			System.out.println(e.getActionCommand());
+			break;
+		case "Montreal":
+			cityChoice = "'Montreal'";
+			selection.setText(productChoice + " | " + cityChoice);
+			update();
+			System.out.println(e.getActionCommand());
+			break;
+		case "Ottawa":
+			cityChoice = "'Ottawa'";
+			selection.setText(productChoice + " | " + cityChoice);
+			update();
+			System.out.println(e.getActionCommand());
+			break;
+		}
 	}
 
 }
